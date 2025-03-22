9b6d74f741da66d58d997d0e5f9b2f24aad8b635
Utsav Ojha <utsavojha@Utsavs-Air.cable.rcn.com>
2025-03-21 21:42:12 -0400

    Terraform configuration

---
+++ infra/bucket.tf
@@ -0,0 +1,67 @@
+resource "aws_s3_bucket" "webbucket" {
+  bucket = "my-tf-web-bucket-19999"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website_security_configs" {
+  bucket = aws_s3_bucket.webbucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.webbucket.id
+  depends_on = [aws_s3_bucket.webbucket]
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.webbucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.webbucket.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.webbucket.id
+  key    = "index.html"
+  source = "../index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "styles_file" {
+  bucket = aws_s3_bucket.webbucket.id
+  key    = "style.css"
+  source = "../style.css"
+  content_type = "text/css"
+}
+
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.webbucket.id
+  key    = "error.html"
+  source = "../error.html"
+  content_type = "text/html"
+}
